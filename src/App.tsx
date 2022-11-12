86c7392397ed8d516e7a4553dc812d12793a3954
Prohor <prohor-yakovlev@mail.ru>
2022-11-12 13:19:01 +0300

    battonCallBack

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber, age, address)
+    }
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
+    }
+    const Button3Foo = (subsriber: string) => {
+        console.log(subsriber)
+    }
+
+    return (
+        <div className="App">
+            {/*<button>*/}
+            {/*    MyYouTubeChanel-1*/}
+            {/*</button>*/}
+            {/*<button>*/}
+            {/*    MyYouTubeChanel-2*/}
+            {/*</button>*/}
+            <Button name={"MyYouTubeChanel-1"} callBack={() => Button1Foo("In Vasya", 21, "Live in Minsk")}/>
+            <Button name={"MyYouTubeChanel-2"} callBack={() => Button2Foo("Im Ivan")}/>
+            <Button name={"MyYouTubeChanel-3"} callBack={() => Button3Foo("Im stupid Button")}/>
+            {/*<Button name={"Delete"}/>*/}
+
+        </div>
+
+    );
 }
 
 export default App;
